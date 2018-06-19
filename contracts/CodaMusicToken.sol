2831880e176965c8422b3996f2cbba2856966e82
YJ <yjkimjunior@gmail.com>
2018-06-19 10:59:09 -0700

    more coda music token js tests

--- contracts/CodaMusicToken.sol
+++ contracts/CodaMusicToken.sol
@@ -1,19 +1,21 @@
 //this standard is based on ethereum
 //https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.19;
 
-import 'openzeppelin-solidity/contracts/token/ERC20/StandardBurnableToken.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract CodaMusicToken is StandardBurnableToken, Ownable {
+
+contract CodaMusicToken is BurnableToken, StandardToken, Ownable, MintableToken {
 
 	string public name;
 	string public symbol;
-	address public owner;
 	uint256 public decimals;
 	uint256 public deployedTime_;
 
-	function CodaMusicToken(string _name, string _symbol, uint256 _decimals, uint256 _total_supply) public Ownable() {
+	function CodaMusicToken(string _name, string _symbol, uint256 _decimals, uint256 _total_supply) public MintableToken() BurnableToken() StandardToken() Ownable() {
 		balances[msg.sender] = _total_supply; // this sets the registry to own all the tokens
 
 		name = _name;
@@ -27,4 +29,19 @@ contract CodaMusicToken is StandardBurnableToken, Ownable {
 	function getDetails() public view returns (string, string, address, address, uint256, uint256, uint256) {
 		return (name, symbol, owner, address(this), decimals, deployedTime_, totalSupply_);
 	}
+
+	modifier onlyOwner() {
+		require(tx.origin == owner);
+		_;
+	}
+
+	/*
+		Overwrite to make sender value tx.origin since this will only be called from the Registry
+
+		alter modifier from Ownable contract to accept tx.origin not msg.sender
+	*/
+	function burn(uint256 _value) public onlyOwner() {
+		_burn(tx.origin, _value);
+	}
+
 }
--- contracts/CodaMusicTokensRegistry.sol
+++ contracts/CodaMusicTokensRegistry.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.19;
 import './CodaMusicToken.sol';
 
 contract CodaMusicTokensRegistry {
@@ -19,12 +19,18 @@ contract CodaMusicTokensRegistry {
 
   /* mapping (user address => token addresses) public owners; */
   mapping (address => address[]) public owners;
-  //mapping of token addresses to mapping of account balances (token=0 means Ether)
+  //mapping of token addresses to mapping of accounts to their balances (token=0 means Ether)
   mapping (address => mapping (address => uint256)) public token_balances;
 
-  address[] public tokens;
+  /* one to one mapping of token to owner */
+  mapping(address => address) public tokenToOwner;
 
   modifier onlyCEO() {
+    _;
+  }
+
+  modifier onlyTokenOwner() {
+    //TODO:
     _;
   }
 
@@ -41,7 +47,7 @@ contract CodaMusicTokensRegistry {
     CodaMusicToken newCodaToken = (new CodaMusicToken(_name, _symbol, _decimals, _total_supply));
 
   	owners[msg.sender].push(newCodaToken);
-    tokens.push(newCodaToken);
+    tokenToOwner[newCodaToken] = msg.sender;
 
     token_balances[newCodaToken][msg.sender] = _total_supply;
 
@@ -66,11 +72,24 @@ contract CodaMusicTokensRegistry {
   }
 
 	function getTokenAtIndex(address token_owner, uint256 token_index) public view returns(string, string, address, address, uint256, uint256, uint256) {
-    assert(token_owner != address(0));
-    assert(token_index >= 0);
+    require(token_owner != address(0));
+    require(token_index >= 0);
 
     CodaMusicToken token = CodaMusicToken(owners[token_owner][token_index]);
 
     return token.getDetails();
 	}
+
+  function burnToken(address _token, uint256 _value) public onlyTokenOwner() {
+    CodaMusicToken(_token).burn(_value);
+  }
+
+  function transferOwnership(address _token, address _newOwner) public {
+    CodaMusicToken(_token).transferOwnership(_newOwner);
+  }
+
+  function mint(address _token, address _to, uint256 _value) public returns (bool) {
+    return CodaMusicToken(_token).mint(_to, _value);
+  }
+
 }
